4475a116a19a4a6c91b73c6da0f5dd7233e5be49
Delowar Hossain <bscdelowar@yahoo.com>
2025-01-11 00:41:15 +0600

    category-brand crud

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,32 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBrandRequest;
+use App\Http\Requests\UpdateBrandRequest;
+use App\Http\Resources\BrandResource;
+use App\Models\Brand;
+use Illuminate\Http\JsonResponse;
 
 class BrandController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $brands = Brand::paginate();
+        return response()->json([
+            'status' => true,
+            'message' => 'Brands retrieved successfully',
+            'data' => $brands,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreBrandRequest $request): JsonResponse
     {
-        //
+        $data = $request->validated();
+
+        if ($request->hasFile('brandImg')) {
+            $data['brandImg'] = $request->file('brandImg')->store('uploads', 'public');
+        }
+
+        $brand = Brand::create($data);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Brand created successfully',
+            'data' => new BrandResource($brand),
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Brand $brand)
     {
-        //
+        return response()->json([
+            'status' => true,
+            'message' => 'Brand retrive successfully',
+            'data' => $brand,
+        ]);
+        ;
     }
 
     /**
@@ -41,8 +68,12 @@ class BrandController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Brand $brand): JsonResponse
     {
-        //
+        $brand->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Brand deleted successfully',
+        ]);
     }
 }
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -11,38 +14,62 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $category = Category::paginate();
+        return response()->json([
+            'status' =>true,
+            'message' =>'Category retrived successfully',
+            'data' =>$category,
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        $category = Category::create($request->validated());
+        return response()->json([
+            'status' =>true,
+            'message' =>'Category created successfully',
+            'data' =>$category,
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        return response()->json([
+            'status' =>true,
+            'message' =>'Category retrive successfully',
+            'data' =>$category,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+
+        $category->update($request->validated());
+        return response()->json([
+            'status' =>true,
+            'message' =>'Category updated successfully',
+            'data' =>$category,
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->json([
+            'status' =>true,
+            'message' =>'Category deleted successfully',
+        ]);
     }
 }
